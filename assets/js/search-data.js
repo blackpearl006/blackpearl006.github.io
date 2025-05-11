dd60735d37451e6a4bf677f5e0f26562aff070ee
blackpearl006 <80585148+blackpearl006@users.noreply.github.com>
2025-05-11 17:21:05 +0000

    Deploying to gh-pages from @ blackpearl006/blackpearl006.github.io@9cada41db2b8795a1eec9bb1b56a5affc820825c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,28 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-understanding-linear-algebra-part-3",
+        },{id: "post-understanding-linear-algebra-part-3",
         
           title: "Understanding Linear Algebra - Part 3",
         
